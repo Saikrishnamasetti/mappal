4a7cbc39feef2f77f73398027e6fca47724078e2
evanfrawley <evanfrawley@gmail.com>
2016-01-30 18:07:15 -0800

    added geolocation

--- js/data.js
+++ js/data.js
@@ -4,6 +4,9 @@ var curr_lat = 36.9719;
 var curr_long = 122.0264;
 var counter = 0;
 
+var person = null;
+//var testsession = null;
+
 Data.schema = new SimpleSchema({
     text: {type: String},
     name: {type: String},
@@ -13,11 +16,14 @@ Data.schema = new SimpleSchema({
     stop_long: {type: Number},
     route_qual: {type: Number},
     route_grade: {type: Number},
+    route_start: {type: String},
+    route_end: {type: String},
     createdAt: {type: Date}
  });
 
 
-function showPosition(position){
+
+/*function showPosition(position){
      Data.insert({
          text: "" + curr_lat + "_" + curr_long + "%%" + position.coords.latitude + "_" + position.coords.longitude,
          name: "test " + counter,
@@ -40,6 +46,7 @@ function tagLocation() {
 
     }
 }
+*/
 
 if(Meteor.isServer)
 {
@@ -50,20 +57,82 @@ if(Meteor.isServer)
 
 if (!Meteor.isServer)
 {
-    Template.data.helpers({
-        tests: function()
-        {
-            return Data.find({}, {sort: {createdAt: -1}});
-        }
-    });
 
     Template.data.events({
-        "click .button": function()
+        "click #start": function()
+        {
+            var ll = Geolocation.latLng();
+            Session.set("curr_long", ll.lng);
+            Session.set("curr_lat", ll.lat);
+            var old_lat = Session.get("curr_lat");
+            var old_long = Session.get("curr_long");
+            var thisLocation = "" + ll.lng + "," + ll.lat;
+            Session.set("locations", thisLocation);
+            Session.set("starting", thisLocation);
+            Session.set("curr_long", ll.lng);
+            Session.set("curr_lat", ll.lat);
+            console.log(thisLocation);
+
+        },
+        "click #checkpoint": function()
+        {
+            var locs = Session.get("locations");
+            var old_lat = Session.get("curr_lat");
+            var old_long = Session.get("curr_long");
+            var latLng = Geolocation.latLng();
+            var thisLocation = "" + latLng.lng + "," + latLng.lat;
+            Session.set("locations", locs + ";" + thisLocation);
+            Data.insert({
+                text: thisLocation,
+                name: thisLocation,
+                start_lat: old_lat,
+                start_long: old_long,
+                stop_lat: latLng.lat,
+                stop_long: latLng.lng,
+                route_qual: 1.0,
+                route_grade: 1.0,
+                route_start: Session.get("starting"),
+                route_end: " ",
+                createdAt: new Date()
+
+            });
+            Session.set("curr_lat", latLng.lat);
+            Session.set("curr_lng", latLng.lng);
+            console.log(locs + ";" + thisLocation);
+            //add in the graph
+
+
+        },
+        "click #stop": function()
         {
 
-            tagLocation();
-            console.log("going through");
-            counter++;
+            var locs = Session.get("locations");
+            var old_lat = Session.get("curr_lat");
+            var old_long = Session.get("curr_long");
+            var latLng = Meteor.call("loc");
+            var thisLocation = "" + latLng.lng + "," + latLng.lat;
+            Session.set("ending", thisLocation);
+            Session.set("locations", locs + ";" + thisLocation);
+            Data.insert({
+                text: thisLocation,
+                name: thisLocation,
+                start_lat: old_lat,
+                start_long: old_long,
+                stop_lat: latLng.lat,
+                stop_long: latLng.lng,
+                route_qual: 1.0,
+                route_grade: 1.0,
+                route_start: Session.get("starting"),
+                route_end: thisLocation,
+                createdAt: new Date()
+
+            });
+            Session.set("curr_lat", latLng.lat);
+            Session.set("curr_lng", latLng.lng);
+            console.log(thisLocation);
+
+            //add in the graph
         }
     });
+
 }
--- js/mappal.js
+++ js/mappal.js
@@ -3,49 +3,49 @@
  */
 
 if (!Meteor.isServer) {
-    function onMove(acc)
-    {
+    function onMove(acc) {
         Session.set("accX", acc.x);
         Session.set("accY", acc.y);
         Session.set("accZ", acc.z);
     }
 
-    function onFail()
-    {
+    function onFail() {
         alert("onFail!");
     }
 
     Template.body.helpers({
-        notMain: function()
-        {
+        notMain: function () {
             return Session.get("view") != "main";
         },
 
-        getView: function()
-        {
+        getView: function () {
             return Template[Session.get("view")];
         }
 
     });
 
     Template.body.events({
-        "click .navigate": function(event)
-        {
+        "click .navigate": function (event) {
             var dest = event.target.id;
             Session.set("view", dest);
             console.log(dest);
-        },
+        }
     });
 
     Meteor.startup(function () {
+
+
         Session.set("view", "main");
         Session.set("sensitivity", 15);
 
         Session.set("accX", 0);
         Session.set("accY", 0);
         Session.set("accZ", 0);
-
-        var accOpts = { frequency: 50 };
+        var accOpts = {frequency: 50};
         //navigator.accelerometer.watchAcceleration(onMove, onFail, accOpts);
+
     });
 }
+
+
+
