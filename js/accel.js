ac7de6169d2abbba05340635be9e3e7e4076e060
Chase Cummings <chaseecummings@gmail.com>
2016-01-30 18:09:15 -0800

    Render acceleration chart

--- js/accel.js
+++ js/accel.js
@@ -1,4 +1,37 @@
-if (Meteor.isCordova) {
+AccelEvents = new Mongo.Collection("accel");
+var watchID;
+
+if (Meteor.isServer) {
+    (function() {
+        var accelEvents = AccelEvents.find({}, { sort: { createdAt: -1 }});
+        accelEvents.observeChanges({
+            added: function(doc) {
+                console.log("Added!");
+            }
+        });
+    })();
+}
+
+if (!Meteor.isServer) {
+    function onMove(acc)
+    {
+        Session.set("accX", acc.x);
+        Session.set("accY", acc.y);
+        Session.set("accZ", acc.z);
+
+        AccelEvents.insert({
+            x: acc.x,
+            y: acc.y,
+            z: acc.z,
+            createdAt: new Date()
+        });
+    }
+
+    function onFail()
+    {
+        alert("onFail!");
+    }
+
     Template.accel.helpers({
         accX: function()
         {
@@ -16,12 +49,17 @@ if (Meteor.isCordova) {
         }
     });
 
-    Template.accel.events({
-        "click .navigate": function(event)
-        {
-            var dest = event.target.id;
-            Session.set("view", dest);
-            console.log(dest);
-        }
+    Template.accel.onRendered(function() {
+        Session.set("accX", 0);
+        Session.set("accY", 0);
+        Session.set("accZ", 0);
+
+        var accOpts = { frequency: 100 };
+        watchID = navigator.accelerometer.watchAcceleration(onMove, onFail, accOpts);
+    });
+
+    Template.accel.onDestroyed(function() {
+        console.log("DESTROYED!!!");
+        navigator.accelerometer.clearWatch(watchID);
     });
 }
